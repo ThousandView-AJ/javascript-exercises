90674097cac4f3f3d73f581e17b9799d791b7203
Dawn <218344099+ThousandView-AJ@users.noreply.github.com>
2026-03-23 08:42:02 +0530

    attempted exercise 06 - repeatString and only read exercise 07 to exercise 11

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(a,b) {
+    if(b<0){
+        return "ERROR"
+    }
+    let out="";
+    for(i=0;i<b;i++){
+
+        out+=a
+    };
+    return out;
+    
 
 };
 
